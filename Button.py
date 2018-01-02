15a20bf5f12da6cbf159e23f4b5492aa543098a6
kai2june <fashion860312@gmail.com>
2018-01-02 22:27:44 +0800

    Update Button.py
    Finished.

--- Button.py
+++ Button.py
@@ -1,33 +1,31 @@
-import pygame
-from pygame.locals import *
-import sys
-import time
+import tkinter as tk
+import globcfg
 
-pygame.init() #初始化pygame
-screen=pygame.display.set_mode([640,480])  #窗口大小：640*480
-screen.fill([255,255,200])#用白色填充窗口
-myReader=pygame.image.load('C:/Users/莊凱鈞/desktop/Reader_priority.png') #把变量myimage赋给导入的图片
-width_reader, height_reader = myReader.get_size()
-myWriter=pygame.image.load('C:/Users/莊凱鈞/desktop/Writer_priority.png')
-width_writer, height_writer = myWriter.get_size()
-screen.blit(myReader,[100,100]) #在100,100的地方画出这个图片（100和100为左部和上部）
-screen.blit(myWriter,[100,300])
-pygame.display.flip()
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x100')
 
-while True:
-    for e in pygame.event.get():#获得事件
-        if e.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if e.type == pygame.MOUSEBUTTONDOWN: #判断鼠标位置以及是否摁了下去。
-            mx, my = pygame.mouse.get_pos()
-            if mx >=100 and mx<=width_reader+100 and my>=100 and my<=height_reader+100:
-            #做需要做的事情，如开始游戏。
-                screen.fill([255,0,0])# Click on Reader_priority
-            elif mx>=100 and mx<=width_writer+100 and my>=300 and my<=height_writer+300:
-                screen.fill([0,0, 255]) #Click on Writer_priority
-            else:
-                screen.fill([0,255,0]) #Click on neither
+var = tk.StringVar()
+l = tk.Label(window, textvariable=var, bg='green', font=('Arial', 12), width=30,
+             height=2)
+#l = tk.Label(window, text='OMG! this is TK!', bg='green', font=('Arial', 12), width=15, height=2)
+l.pack()
 
-    #time.sleep(0.5)
-    pygame.display.update()
+var.set('Writer_priority')
+on_hit = False
+def prior():
+    global on_hit
+    if on_hit == False:
+        globcfg.priority = 'Reader_priority'
+        on_hit = True
+        var.set('Reader_priority')
+    else:
+        globcfg.priority = 'Writer_priority'
+        on_hit = False
+        var.set('Writer_priority')
+
+b = tk.Button(window, text='Writer/Reader (default:writer)', width=30,
+              height=2, command=prior)
+b.pack()
+
+window.mainloop()
