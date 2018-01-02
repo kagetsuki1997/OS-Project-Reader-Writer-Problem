c068d960efb0f5310185b14d11a56ea500c4b6e4
kai2june <fashion860312@gmail.com>
2018-01-02 22:29:13 +0800

    Create Input_box_lambda.py
    Finished.

---
+++ Input_box_lambda.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import globcfg
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+
+e = tk.Entry(window)
+e.pack()
+def insert_point():
+    var = e.get()
+    globcfg.lamGen = int(var)
+    t.insert('insert', var)
+
+b1 = tk.Button(window, text='Enter lamGen value', width=15,height=2, command=insert_point)
+b1.pack()
+t = tk.Text(window, height=5)
+t.pack()
+
+e2 = tk.Entry(window)
+e2.pack()
+def insert_point2():
+    var = e2.get()
+    globcfg.lamRW = int(var)
+    t2.insert('insert', var)
+    return var
+b2 = tk.Button(window, text='Enter lamRW value', width=15, height=5, command= insert_point2)
+b2.pack()
+t2 = tk.Text(window, height=9)
+t2.pack()
+
+window.mainloop()
