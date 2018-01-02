2a08d2c091a27392f0a4445bf1d15d690650ae14
kai2june <fashion860312@gmail.com>
2018-01-02 22:30:48 +0800

    Update globcfg.py
    Finished.

--- globcfg.py
+++ globcfg.py
@@ -1,7 +1,9 @@
 waitingList=[]
 starveThreshold=5
-priority='Writer'
+priority='Writer_priority'
 threadNumber=0
 currentRunThreadCount={'Writer':0,'Reader':0} #the thread which is "start"
-lamGen=10 #average time is lamGen/10
-lamRW=50 #average time is lamRW/10
+global lamGen
+lamGen = 10 #average time is lamGen/10
+global lamRW
+lamRW = 50 #average time is lamRW/10
