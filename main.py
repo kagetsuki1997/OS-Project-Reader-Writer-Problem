c14ec77e41be4c1252cabe353a7a3385b595416d
kai2june <fashion860312@gmail.com>
2018-01-02 22:31:45 +0800

    Update main.py
    Finished.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import SystemControl,Book,globcfg
+import Input_box_lambda
+import Button
 
 def main():
+    Input_box_lambda
+    print ("lamGen= %d" %globcfg.lamGen)
+    print ("lamRW= %d" %globcfg.lamRW)
+    Button
+    print ("Priority: %s" %globcfg.priority)
     book=Book.Book()
     generator=SystemControl.Generator(book)
     scheduler=SystemControl.Scheduler()
